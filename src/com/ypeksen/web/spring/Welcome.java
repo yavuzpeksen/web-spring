32c86e2493760a06a77fe5f5ee19a1307928f5f5
Yavuz <yavuzpeksen1986@hotmail.com>
2016-12-17 10:20:01 +0300

    Initial stage for web spring

---
+++ src/com/ypeksen/web/spring/Welcome.java
@@ -0,0 +1,29 @@
+package com.ypeksen.web.spring;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class Welcome {
+ 
+	
+	@RequestMapping("welcome/")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+	@RequestMapping("showname/")
+	public ModelAndView show(@RequestParam("name") String name, HttpServletRequest request) {
+ 
+		System.out.println("Name is: " + name);
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+}
